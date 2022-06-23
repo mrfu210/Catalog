b02e19e9caabbf21e39b0984be634d493f5b27e5
mrfu210 <scutaru.georgian@yahoo.ro>
2022-06-23 23:49:49 +0100

    ADD POST, PUT, DELETE

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -42,5 +42,57 @@ namespace Catalog.Cotrollers
             }
             return item.AsDto();
         }
+
+        //Post /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new(){
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem),new{id= item.Id}, item.AsDto());
+        }
+
+        //PUT / items/{id}
+        [HttpPut]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with 
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+        //Delete / items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
+        }
     }
 }
