c45ad303f9fac541f2bb93746311c9d5e9f11a02
mrfu210 <scutaru.georgian@yahoo.ro>
2022-08-03 23:10:39 +0100

    Add password and user name

--- obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("abdc3484-9399-4268-8cbc-51f34102a3a4")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
